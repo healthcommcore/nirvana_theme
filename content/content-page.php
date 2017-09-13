b4462488454dd9755ecbdc57caa94107cea0540d
hccadmin <dave_rothfarb@dfci.harvard.edu>
2017-09-13 14:12:18 -0400

    Removed title from homepage

--- content/content-page.php
+++ content/content-page.php
@@ -10,9 +10,10 @@
 if ( have_posts()  ) while ( have_posts() ) : the_post(); ?>
 
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( is_front_page() ) { ?>
-						<h2 class="entry-title"><?php the_title(); ?></h2>
-					<?php } else { ?>
+					<?php //if ( is_front_page() ) { ?>
+						<!-- <h2 class="entry-title"><?php //the_title(); ?></h2 -->
+					<?php //} else { ?>
+					<?php if ( !is_front_page() ) { ?>
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 					<?php } ?>
 
